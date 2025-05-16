dc4a9e09e413b41aaee81c46b142d2193c84ab77
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-16 19:52:25 +0800

    feat(handler): add recipe data

--- handlers/pageData.handler.php
+++ handlers/pageData.handler.php
@@ -5,4 +5,68 @@
         ["Label"=> "Recipe","url"=> "/pages/recipe/index.php"],
     ];
 
+    
+    $recipe=[
+        [
+            "title" => "Sisig",
+            "time" => "45 Minutes",
+            "ingredients" => [
+                "Pork face and ears",
+                "Onions",
+                "Calamansi",
+                "Chili peppers",
+                "Salt and pepper"
+            ],
+            "image" => ""
+        ],
+        [
+            "title" => "Tocino",
+            "time" => "3 days (marinating) + 20 minutes (cooking)",
+            "ingredients" => [
+                "Pork slices",
+                "Sugar",
+                "Salt",
+                "Anise wine",
+                "Annatto powder",
+                "Garlic",
+                "Pineapple juice",
+                "Curing salt (optional)"
+            ],
+            "image" => ""
+        ],
+        [
+            "title" => "Pork Asado (Kapampangan)",
+            "time" => "1 hour",
+            "ingredients" => [
+                "Pork belly or shoulder",
+                "Soy sauce",
+                "Brown sugar",
+                "Star anise",
+                "Garlic",
+                "Onion",
+                "Bay leaves",
+                "Pepper",
+                "Water"
+            ],
+            "image" => ""
+        ],
+        [
+        "title" => "Bringhe",
+        "time" => "1 hour",
+        "ingredients" => [
+            "Glutinous rice",
+            "Coconut milk",
+            "Chicken",
+            "Chorizo",
+            "Bell peppers",
+            "Raisins",
+            "Turmeric",
+            "Onion",
+            "Garlic"
+        ],
+        "image" => "bringhe.jpg"
+    ],
+    ]
+
+
 ?>
