cdc65ea2ec5f7ea9cfee7bc7ce7a6f91ea5e3b2f
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-18 20:17:09 +0800

    feat(recipe):
    - copy the hero section outline from landing page
    - change the content

--- pages/recipe/index.php
+++ pages/recipe/index.php
@@ -6,6 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+
+    <section class="hero">
+        <div class="hero-overlay">
+            <div class="hero-content">
+                <h1 class="display-5 fw-bold">Kapampangan Food</h1>
+                <p class="lead">See the ingredients and the process to make this culture rich food</p>
+            </div>
+        </div>
+    </section>
+
 </body>
 </html>
