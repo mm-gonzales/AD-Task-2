284f6163e97485a4d8960a5c828ac80f262a31b5
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-18 19:48:19 +0800

    refactor(nav): update toggler and navigation structure for improved functionality

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -4,12 +4,15 @@
 
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-3 px-5" style="position: absolute; top: 0; width: 100%; z-index: 1000;">
   <a class="navbar-brand fw-bold text-white pe-4" href="#">Lasa Local</a>
-  <button class="navbar-toggler text-white border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+
+  <!-- Toggler for Offcanvas -->
+  <button class="navbar-toggler text-white border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-3">
+  <!-- Normal Nav (Desktop Only) -->
+  <div class="d-none d-lg-flex ms-auto align-items-center">
+    <ul class="navbar-nav mb-0 me-3">
       <li class="nav-item">
         <a class="nav-link active text-white px-3" href="#">Home</a>
       </li>
@@ -17,6 +20,8 @@
     <a href="#" class="btn btn-warning text-white fw-semibold px-4">View Recipe</a>
   </div>
 </nav>
+
+
 
 <?php
     }
