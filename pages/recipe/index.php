1382dfda6549ad1c4bb186643007dba6d266780c
mm-gonzales <michaela.v.gonzales@gmail.com>
2025-05-18 21:56:53 +0800

    refactor(recipe): remove unnecessary blank lines in index.php

--- pages/recipe/index.php
+++ pages/recipe/index.php
@@ -34,8 +34,6 @@
         <?php displayRecipeCards($recipe); ?>
     </section>
 
-
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
